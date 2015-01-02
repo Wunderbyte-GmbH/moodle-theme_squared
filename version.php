fceb9cedaf2b36009e7820e826797b3c77ed4edc
David Bogner <davidbogner@gmail.com>
2015-01-03 00:00:44 +0100

    maturity added

--- version.php
+++ version.php
@@ -13,6 +13,7 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2015010200; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100; // Requires this Moodle version
 $plugin->release   = "Red Square 2.7"; // Release name: famous squares around the world followed by Moodle version 
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_squared'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array(
     'theme_canvas'  => 2013050100,
