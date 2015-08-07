ad96effc786360c0dc0024b54e0deef1f82a6929
Gareth J Barnard <gjbarnard@gmail.com>
2015-08-07 20:59:01 +0100

    Closer.

--- amd/src/custom.js
+++ amd/src/custom.js
@@ -26,14 +26,13 @@
  */
 
 /* jshint ignore:start */
-define(['jquery', 'theme_squared/jqueryeasing', 'theme_squared/jqueryflexslider', 'core/log'], function($, e, fs, log) {
+define(['jquery', 'theme_squared/jqueryeasing', 'theme_squared/jqueryflexslider', 'theme_squared/infieldlabels', 'theme_squared/konami', 'core/log'], function($, e, fs, ifl, k, log) {
 
     "use strict"; // jshint ;_;
 
-    console.log('Squared custom AMD');
+    console.log('Squared Custom AMD');
 
-    (function($){$.InFieldLabels=function(b,c,d){var f=this;f.$label=$(b);f.label=b;f.$field=$(c);f.field=c;f.$label.data("InFieldLabels",f);f.showing=true;f.init=function(){f.options=$.extend({},$.InFieldLabels.defaultOptions,d);if(f.$field.val()!=""){f.$label.hide();f.showing=false};f.$field.focus(function(){f.fadeOnFocus()}).blur(function(){f.checkForEmpty(true)}).bind('keydown.infieldlabel',function(e){f.hideOnChange(e)}).change(function(e){f.checkForEmpty()}).bind('onPropertyChange',function(){f.checkForEmpty()})};f.fadeOnFocus=function(){if(f.showing){f.setOpacity(f.options.fadeOpacity)}};f.setOpacity=function(a){f.$label.stop().animate({opacity:a},f.options.fadeDuration);f.showing=(a>0.0)};f.checkForEmpty=function(a){if(f.$field.val()==""){f.prepForShow();f.setOpacity(a?1.0:f.options.fadeOpacity)}else{f.setOpacity(0.0)}};f.prepForShow=function(e){if(!f.showing){f.$label.css({opacity:0.0}).show();f.$field.bind('keydown.infieldlabel',function(e){f.hideOnChange(e)})}};f.hideOnChange=function(e){if((e.keyCode==16)||(e.keyCode==9))return;if(f.showing){f.$label.hide();f.showing=false};f.$field.unbind('keydown.infieldlabel')};f.init()};$.InFieldLabels.defaultOptions={fadeOpacity:0.5,fadeDuration:300};$.fn.inFieldLabels=function(c){return this.each(function(){var a=$(this).attr('for');if(!a)return;var b=$("input#"+a+"[type='text'],"+"input#"+a+"[type='password'],"+"textarea#"+a);if(b.length==0)return;(new $.InFieldLabels(this,b[0],c))})}})(jQuery);
-
+    /*
     (function($) {
 
         var callback = function() { };
@@ -57,6 +56,7 @@ define(['jquery', 'theme_squared/jqueryeasing', 'theme_squared/jqueryflexslider'
         };
 
     })(jQuery);
+    */
 
     $(document).ready(function(){
 
---
+++ amd/src/konami.js
@@ -0,0 +1,27 @@
+/* jshint ignore:start */
+define(['jquery', 'core/log'], function($, log) {
+
+    "use strict"; // jshint ;_;
+
+    log.debug('Squared Konami AMD');
+
+    var callback = function() { };
+    // [up, up, down, down, left, right, left, right, b, a];
+    var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    var i = 0;
+
+    $.fn.konami = function(f) {
+        if (typeof f == 'function') callback = f;
+            return this.keydown(function(e) {
+            // Increment i if the key pressed matched the next key in the sequence.
+            // if not, i goes back to zero, so you can start the code again.
+            i = (e.keyCode == code[i]) ? i + 1 : 0;
+            
+            // if i reaches the length of the code array, the code must have been entered properly
+            if (i == code.length) {
+                typeof callback == 'function' && callback();
+                i = 0;
+            }
+        });
+    };
+});
