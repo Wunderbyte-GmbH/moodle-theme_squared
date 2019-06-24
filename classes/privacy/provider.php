9d9e4c5bc8bf950127dde7508bd31a97ce6943b3
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2019-06-24 11:59:23 +0100

    Add privacy.

---
+++ classes/privacy/provider.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This is the squared theme.
+ *
+ * The squared theme makes uses a custom version of squared blocks
+ *
+ * @package theme_squared
+ * @copyright 2019 onwards Onlinecampus Virtuelle PH
+ * www.virtuelle-ph.at, David Bogner www.edulabs.org
+ * @author G J Barnard - {@link http://moodle.org/user/profile.php?id=442195}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace theme_squared\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The Squared theme does not store any user data.
+ * But if you use CDN fonts then investigate the respective privacy policies.
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:nop';
+    }
+}
