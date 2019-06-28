7797351524be14710e5d912d638e6c382e2bcc80
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2019-06-28 13:27:30 +0100

    V3.6.1.0

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
  */
 defined ( 'MOODLE_INTERNAL' ) || die ();
 
-$plugin->version = 2019061400;
+$plugin->version = 2019061401;
 $plugin->requires  = 2018120301.00; // Moodle 3.6.1 (Build: 20181205).
-$plugin->release = "Heldenplatz 3.6.0.1"; // Release name: Famous squares around the world followed by Moodle version followed by stability number (> 1 = stable) followed by revision.
-$plugin->maturity = MATURITY_BETA;
+$plugin->release = "Heldenplatz 3.6.1.0"; // Release name: Famous squares around the world followed by Moodle version followed by stability number (> 1 = stable) followed by revision.
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_squared'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array (
     'theme_boost' => 2018120300
